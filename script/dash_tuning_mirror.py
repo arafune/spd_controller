8c2383c1abbf08424b5a06aaf952abd694ec8c5a
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-01-10 15:24:00 +0900

    Make a function to build a html.Div component

--- script/dash_tuning_mirror.py
+++ script/dash_tuning_mirror.py
@@ -18,6 +18,113 @@ app = dash.Dash(
     suppress_callback_exceptions=True,
 )
 
+
+def flipper_component(id: int):
+    """Flipper flipper_component
+
+    Parameters
+    ------------
+    id : int
+        1 or 2
+    """
+    return html.Div(
+        [
+            html.H2("Flipper {}".format(id), style={"marginLeft": "2em"}),
+            dbc.Button(
+                "Flip {}!".format(id),
+                size="large",
+                color="primary",
+                id="flipper{}".format(id),
+                style={
+                    "marginLeft": "10em",
+                    "marginBottom": ".3em",
+                },
+            ),
+        ],
+        style={
+            "margin": "2em",
+            "border-style": "solid",
+            "border-radius": "20pt",
+            "border-color": "green",
+        },
+    )
+
+
+def mirror_component(id: int):
+    """Mirror flipper_component
+
+    Parameters
+    -------------
+    id: int
+        1 or 3
+    """
+    html.Div(
+        [
+            html.H3(f"Mirrror {id}ω", style={"marginLeft": "2em"}),
+            daq.LEDDisplay(
+                value="0",
+                color="red",
+                id=f"position_{id}omega",
+                size=24,
+                style={"marginLeft": "1em"},
+            ),
+            dbc.Button(
+                "◀",
+                color="primary",
+                size="sm",
+                style={"marginLeft": "2em"},
+                id=f"left_{id}omega",
+            ),
+            dbc.Button(
+                "stop",
+                color="primary",
+                size="sm",
+                id=f"stop_{id}omega",
+            ),
+            dbc.Button(
+                "▶",
+                color="primary",
+                size="sm",
+                id=f"right_{id}omega",
+            ),
+            html.Div(
+                [
+                    dbc.Input(
+                        type="relative move",
+                        step=1,
+                        placeholder="Input steps from here",
+                        inputmode="numeric",
+                        id=f"move_{id}omega",
+                        style={
+                            "display": "inline-block",
+                            "width": "10em",
+                        },
+                    ),
+                    dbc.Button(
+                        "Move",
+                        color="primary",
+                        id=f"move_start_{id}omega",
+                        style={
+                            "display": "incline-block",
+                            "marginLeft": "1em",
+                        },
+                    ),
+                ],
+                style={
+                    "text-align": "center",
+                    "marginBottom": ".3em",
+                },
+            ),
+        ],
+        style={
+            "border-style": "solid",
+            "border-radius": "20pt",
+            "border-color": "red",
+            "margin": "2em",
+        },
+    )
+
+
 app.layout = html.Article(
     children=[
         html.H1("Tuning Mirrors", style={"text-align": "center"}),
