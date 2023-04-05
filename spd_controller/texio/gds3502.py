be35309ebe47dd87b28600366168d91097d5c141
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-04-05 10:16:52 +0900

    Update gds3502.py

--- spd_controller/texio/gds3502.py
+++ spd_controller/texio/gds3502.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
-"""Module for GDS3502 Oscilloscope (USB connection through pySerial)"""
+"""Module for GDS3502 Oscilloscope (socket connection)"""
 
 from __future__ import annotations
 
-from  typing_extensions import Literal
+import socket
 
 import numpy as np
 from numpy.typing import NDArray
+from typing_extensions import Literal
 
-from .. import Comm
+from .. import SocketClient
 
 Channel = Literal[1, 2]
 
-class GDS3502(Comm):
-    def __init__(self, term: str = "\n") -> None:
+
+class GDS3502(SocketClient):
+    def __init__(
+        self, host: str = "144.213.126.10", port: int = 3000, term: str = "\n"
+    ) -> None:
         """_summary_
 
         Parameters
@@ -27,15 +31,11 @@ class GDS3502(Comm):
         RuntimeError
             _description_
         """
-        super().__init__(term=term)
-        port = self.connect("GEV150786")
-        self.header_dict: dict[str, float | str] = {}
+        super().__init__(host=host, port=port, term=term)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        self.header: dict[str, float | str] = {}
         self.memory: NDArray[np.float_]
-        if port:
-            self.open(baud=115200, port=port)
-            self.is_portopen = True
-            return None
-        raise RuntimeError("Not found")
 
     def lrn(self) -> str:
         """Returns the oscilloscope settings as a data string
@@ -46,11 +46,7 @@ class GDS3502(Comm):
             Oscilloscope setting
         """
         self.sendtext("*LRN?")
-        tmp: list[bytes] = self.comm.readlines()
-        return_info: bytearray = bytearray()
-        for a in tmp:
-            return_info += a
-        return return_info.decode("utf-8")
+        return self.recvline(128)
 
     def acquire_memory(self, channel: Channel) -> NDArray:
         """Return the memory
@@ -66,28 +62,24 @@ class GDS3502(Comm):
             Oscilloscope data
         """
         self.sendtext(":ACQuire{}:MEMory?".format(channel))
-        tmp: list[bytes] = self.comm.readlines()
-        result: bytearray = bytearray()
-        for a in tmp:
-            result += a
-        data_index: int = 7 + result.find(b"#550000")
-        header = result[:data_index].decode("utf-8")
-        wave_data = result[data_index:-1]
+        result = self.recvline(8192)
+        data_index: int = result.find("#550000") + 7
+        header = result[:data_index]
+        wave_data = result[data_index:-1].encode("utf-8")
         for i in header.split(";")[:-2]:
             k, v = i.split(",")
             try:
-                self.header_dict[k] = int(v)
+                self.header[k] = int(v)
             except ValueError:
                 try:
-                    self.header_dict[k] = float(v)
+                    self.header[k] = float(v)
                 except ValueError:
-                    self.header_dict[k] = v
+                    self.header[k] = v
         wave = [[wave_data[i], wave_data[i + 1]] for i in range(0, len(wave_data), 2)]
         timescale: NDArray[np.float64] = np.linspace(
             0,
-            float(self.header_dict["Sampling Period"])
-            * float(self.header_dict["Memory Length"]),
-            int(self.header_dict["Memory Length"]),
+            float(self.header["Sampling Period"]) * float(self.header["Memory Length"]),
+            int(self.header["Memory Length"]),
             endpoint=False,
         )
         amplitudes: list[int] = []
@@ -98,6 +90,6 @@ class GDS3502(Comm):
                 amplitudes.append(-1 * (int(bin(~points[1] & 0xFF), 0)))
         self.timescale = timescale
         self.memory = np.array(
-            np.array(amplitudes) * self.header_dict["Vertical Scale"] / 25
+            np.array(amplitudes) * self.header["Vertical Scale"] / 25
         )
         return self.memory
